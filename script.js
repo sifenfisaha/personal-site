3f72686e9bf8d2b3008e0fc59d6f479c198e5af1
sifenfisaha <168304823+sifenfisaha@users.noreply.github.com>
2024-11-02 21:05:58 +0300

    added social media links and fixex some buges

--- script.js
+++ script.js
@@ -3,24 +3,22 @@ const themeSwich = document.getElementById("theme-swich");
 
 const enableLightmode = () => {
   document.body.classList.add("light-mode");
-  localStorage.setItem("light-mode", "active"); // Update localStorage
+  localStorage.setItem("light-mode", "active");
 };
 
 const disableLightmode = () => {
   document.body.classList.remove("light-mode");
-  localStorage.setItem("light-mode", "inactive"); // Update localStorage
+  localStorage.setItem("light-mode", "inactive");
 };
 
-// Check saved mode on load
 if (lightmode === "active") {
   enableLightmode();
 } else {
   disableLightmode();
 }
 
-// Toggle mode on button click
 themeSwich.addEventListener("click", () => {
-  lightmode = localStorage.getItem("light-mode"); // Refresh the value
+  lightmode = localStorage.getItem("light-mode");
   if (lightmode !== "active") {
     enableLightmode();
   } else {
